36dcdbc8fac84f60f1ffaddd18b81944d541a5d0
Abdulhalık İşcan <210304008@siirt.edu.tr>
2022-10-18 11:40:07 +0300

    a

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {colors:{
+    extend: {
+      colors:{
       "ay-gray" : "hsl(220,15%,55%)",
       "koyu-mavi" : "hsl(218,44%,22%)",
+        "nftmainbgcolor" : "hsl(217, 54%, 11%)",
+        "nftcardbackground" : "hsl(216, 50%, 16%)",
+        "nfttxtcolor" : "hsl(215, 51%, 70%)",
+        "nftlinecolor" : "hsl(215, 51%, 70%)",
     }
     },
   },
